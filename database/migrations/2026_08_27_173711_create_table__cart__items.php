85c5dd186eef82e946b01ddfbf46f777ded07202
Santiago Ruiz <sruiz557@unab.edu.co>
2026-08-27 12:47:44 -0500

    migracion user products y cart Items

---
+++ database/migrations/2026_08_27_173711_create_table__cart__items.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('table__cart__items', function (Blueprint $table) {
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('table__cart__items');
+    }
+};
