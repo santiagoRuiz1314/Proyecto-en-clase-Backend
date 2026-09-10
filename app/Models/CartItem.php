1c32fe46d45ffebfb7e70c6979cd7ab387f7dc47
Santiago Ruiz <sruiz557@unab.edu.co>
2026-09-10 11:40:12 -0500

    Modelos

---
+++ app/Models/CartItem.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CartItem extends Model
+{
+    protected $table = 'table__cart__items';
+    protected $primaryKey = 'id';
+}
