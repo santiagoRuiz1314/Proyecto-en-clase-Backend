5d266ddd470c05909da6276cbf974674df2ca6e9
Santiago Ruiz <sruiz557@unab.edu.co>
2026-09-10 11:59:10 -0500

    Factory Category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $table = 'categories';
+    use HasFactory;
+    protected $table = 'table_categories';
     protected $primaryKey = 'id';
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,9 +18,17 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+
+        $category1 = new Category();
+        $category1->name = 'Tecnologia';
+        $category1->description = 'Productos tecnologicos';
+        $category1->save();
+
+        $category2 = new Category();
+        $category2->name = 'Ropa';
+        $category2->description = 'Productos de ropa';
+        $category2->save();
+
+        Category::factory(1000)->create();
     }
 }
